5e0ddc4d82d7a584999c356a6e7aee19ebbc9f77
avinashbiradar <avinashbiradar179@gmail.com>
2021-02-01 09:43:36 +0530

    Added:UI for quantity measurement

---
+++ script/QuantityMeasurement.js
@@ -0,0 +1,50 @@
+const optionValueOne = ["Meter", "Kilometre", "Centimeters", "Millimetre", "Micrometre", "Mile", "Foot", "Inch"]
+const optionValueTwo = ["Celsius", "Farenhite", "Kalvin"];
+const optionValueThree = ["Litres", "Mililitres", "Gallons"];
+
+
+
+
+
+
+// var input = document.getElementById('input');
+// var result = document.getElementById('result');
+// var inputType = document.getElementById('inputType');
+// var resultType = document.getElementById('resultType');
+// var inputTypeValue,resultTypeValue;
+
+// // now add listener
+// input.addEventListener("keyup",myResult);
+// inputType.addEventListener("change",myResult);
+// resultType.addEventListener("change",myResult);
+
+// // taking initial value
+// inputTypeValue = inputType.value;
+// resultTypeValue = resultType.value;
+
+
+// function myResult(){
+
+
+// // when we change the input and output type vale we need to updata the 
+// // inputTypeValue and resultTypeValue
+
+// 	inputTypeValue = inputType.value;
+// 	resultTypeValue = resultType.value;
+
+
+// // now compare the input and resultType value and add formula
+
+// 	if(inputTypeValue === "meter" && resultTypeValue==="kilometer"){
+
+// 		//this is meter to kilometer formula 
+// 		result.value = Number(input.value) * 0.001;
+// 	}else if(inputTypeValue === "meter" && resultTypeValue==="Centimeter"){
+
+// 		//this is meter to Centimeter formula 
+// 		result.value = Number(input.value) * 100;
+
+// 	}else if(inputTypeValue === "meter" && resultTypeValue==="meter"){
+// 		//this is meter to meter formula 
+// 		result.value = input.value
+// 	}
